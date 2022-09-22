f080ce4b14b12f6c54c578a1305b35cc6451b77d
Muflih Maxi <muflihmaxi@gmail.com>
2022-09-22 19:34:02 +0700

    Tugas 4

---
+++ todolist/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from todolist.models import Task
+
+@login_required(login_url='/todolist/login/')
+def show_todolist(request):
+    data_todolist = Task.objects.all()
+
+    context = {
+        'list_todolist': data_todolist,
+    }
+    return render(request, "todolist.html", context)
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('todolist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("todolist:show_todolist")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('todolist:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def create_task_user(request):
+    if request.method == 'POST':
+        task_baru = Task()
+        task_baru.user = request.user
+        task_baru.date = str(datetime.date.today())
+        task_baru.title = request.POST.get('title')
+        task_baru.description = request.POST.get('description')
+        task_baru.save()
+    context = {}
+    return render(request, 'create-task.html', context)
